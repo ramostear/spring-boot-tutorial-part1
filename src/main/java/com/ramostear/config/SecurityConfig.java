d3debc62e7959e54ce5a5179bebca63d30179e45
ramostear <ramostear@163.com>
2019-03-03 06:39:49 +0800

    spring boot 2.0 tutorial part 1

--- src/main/java/com/ramostear/config/SecurityConfig.java
+++ src/main/java/com/ramostear/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.ramostear.config;
 
-/*
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-*/
 
 /**
  * @author ramostear
@@ -13,14 +11,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @modify by :
  * @since:
  */
-/*@Configuration
-@EnableWebSecurity*/
-public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/{
-    /*@Override
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll()
                 .and().cors().disable();
-    }*/
+    }
 }
